b6d2815dcd4c8bebcbe494bf1b2376a0f84e9c71
Salman Farshe <salmanfarshe39@gmail.com>
2021-10-25 22:17:22 +0600

    doing some configuration

---
+++ app.js
@@ -0,0 +1,22 @@
+// initialize packages
+let express             = require("express"),
+    app                 = express(),
+    port                = 3000,
+    bodyParser          = require("body-parser"),
+    methodOverWrite     = require('method-override');
+
+// ====================================== config =================================
+// don't need to use .ejs extention
+app.set("view engine", "ejs")
+// tell express to serve the content of the public directory
+app.use(express.static("public"));
+// retrive data from the body
+app.use(bodyParser.urlencoded({extended: true}));
+// method override
+app.use(methodOverWrite("_method"));
+
+// ===================================== mongo DB config ==========================
+ let mongoose           = require("mongoose");
+ mongoose.connect("mongodb://localhost/crud_app");  // connect to the db & create crud_app. if exists then it'll use it. otherwise created automatically.
+
+ 
